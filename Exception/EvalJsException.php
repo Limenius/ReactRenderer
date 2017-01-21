9e7a652a3f3394965b44f94c62f90856c7984a28
Nacho Martín <nacho@limenius.com>
2017-01-21 18:21:37 +0100

    Initial code porting code from React Bundle

---
+++ Exception/EvalJsException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Limenius\ReactRenderer\Exception;
+
+/**
+ * Class EvalJsException
+ */
+class EvalJsException extends \RuntimeException
+{
+    /**
+     * EvalJsException constructor.
+     *
+     * @param string $componentName
+     * @param int    $consoleReplay
+     */
+    public function __construct($componentName, $consoleReplay)
+    {
+        $message = 'Error rendering component '.$componentName."\nConsole log:".$consoleReplay;
+        parent::__construct($message);
+    }
+}
